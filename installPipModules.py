e687848905a30790c1a44b41d83c0c08f346d611
Alum1n <152102143+Alum1n@users.noreply.github.com>
2024-05-29 18:54:19 +0200

    Update installPipModules.py to support Debian

--- installPipModules.py
+++ installPipModules.py
@@ -4,5 +4,12 @@ if sys.platform.startswith("linux"):
     if distro["NAME"] == "Arch Linux":
         os.system("sudo pacman -S python-pip")
         os.system("pip install colorama pynput --break-system-packages")
+    elif distro["NAME"] == "Debian":
+        os.system("apt install python3-pip")
+        os.system("pip install colorama pynput --break-system-packages")
     else:
-        print("imagine not using arch")
+        print("This only supports Arch Linux and Debian")
+        print("The pip modules needed are: colorama, pynput")
+else:
+    print("This only supports Arch Linux and Debian")
+    print("The pip modules needed are: colorama, pynput")
